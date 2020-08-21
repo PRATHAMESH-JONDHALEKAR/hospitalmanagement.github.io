67568e79745987369b058e33971b04c40cd21803
Prathamesh Jondhalekar <prathamesh8478@gmail.com>
2020-08-21 20:11:13 +0530

    hms
    hms click

---
+++ hospital management systems/src/tf5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+class tf5 {
+
+    static void setText(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
